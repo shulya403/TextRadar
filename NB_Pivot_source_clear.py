be6af6fef14d52986fe2ad57c94f375e1fa0e956
shulya403 <56358220+shulya403@users.noreply.github.com>
2020-01-04 19:30:34 +0300

    Text Radar c формулой вывода [x[2]**(x[2]-1)/квадрат длинн слов

--- NB_Pivot_source_clear.py
+++ NB_Pivot_source_clear.py
@@ -9,76 +9,79 @@ from pprint import pprint
 import StRadar
 
 class nb_excel_clear(object):
-    
+
     def __init__(self, filename, fileresult):
-        
+
         self.filename = str(filename)
         self.outfile = str(fileresult)
         self.df = pd.read_excel(self.filename)
         self.df.Brand = self.df.Brand.str.lower()
         self.brands = self.df.Brand.unique()
-       
+
         #Списки известных моделей по брендам
-        
-        self.brand_models_dict = dict()
+
+        self.brand_models_dict = dict() #словарь бренды : известные модели
 
         for br in self.brands:
-            
-            self.brand_models_dict[br] = self.df[(self.df['Brand'] == br) & 
+
+            self.brand_models_dict[br] = self.df[(self.df['Brand'] == br) &
                                   (self.df['TM'].isnull() == False)]['Model'].unique()
-            
+
         self.df['Cheked'] = 0
-        
+
     def searcher(self, df_line):
-        
-        cons_estim_dict = dict()
+
+        cons_estim_dict = dict() # словарь известные модели данного бренда : оценка stradar
         for i in self.brand_models_dict[df_line['Brand']]:
             cons_estim_dict[i] = StRadar.stradar(df_line['Source'], i).result()
-        
 
-        max_estim = max(cons_estim_dict.values())
-        print('max', max_estim)
-        
-        max_estim_dict = {k:v for k,v in cons_estim_dict.items() if v == max_estim}
+
+        max_estim = max(cons_estim_dict.values()) #максимум оценки по известным названиям модлей
+        # словарь меделей имеющих максимальное значение
+        max_estim_dict = {k: v for k, v in cons_estim_dict.items() if v == max_estim}
         pprint(max_estim_dict)
-        
-        
+        pprint(cons_estim_dict)
+
+        # Выбираем самое длинное название модели из тех которые дали макисмум совпадения
         if len(max_estim_dict) > 1:
             max_len = max([len(str(l)) for l in max_estim_dict.keys()])
             longest_names = [k for k in max_estim_dict.keys() if len(str(k)) == max_len]
-            
+
         else:
             longest_names = list(max_estim_dict.keys())
-        
+
         estim_model_name = str(longest_names[0])
-        
+
         df_line['Model'] = estim_model_name
         df_line['Cheked'] = 1
-        try:    
+
+        #прибиваем название торговой марки, беря ее из описания известных моделей из основного df
+        try:
             TM = self.df[(self.df['Cheked'] == 0) & (self.df['Model'] == df_line['Model'])]['TM'].iloc[0]
         except IndexError:
             TM = ''
 
         df_line['TM'] = TM
         print(df_line)
-        
+
         return df_line
-    
+
     def nec_output(self):
-        
+
         self.df.to_excel(self.outfile)
-        
+
     def fill_na(self):
+        # Пускает apply self.searcher для незапоплненных строчек
        self.df[self.df['TM'].isnull()] = self.df[
-               self.df['TM'].isnull()].apply(lambda f: self.searcher(f), axis = 1)
-       
-       
+               self.df['TM'].isnull()].apply(lambda f: self.searcher(f), axis=1)
+
+
        self.nec_output()
-    
 
-nb_excel_clear('NB_Pivot_November_19_py.xlsx', 'NB_Pivot_November_19_1.xlsx').fill_na() 
 
-        
+nb_excel_clear('NB_Pivot_November_19_py.xlsx', 'NB_Pivot_stradar_x-st-x.xlsx').fill_na()
+
+
 
 
 
--- StRadar.py
+++ StRadar.py
@@ -6,6 +6,8 @@ Created on Tue Dec 17 22:58:24 2019
 """
 
 import numpy as np
+from pprint import pprint
+import math
 
 class stradar(object):
     
@@ -18,7 +20,7 @@ class stradar(object):
             for j in range(self.search_len):
                 if (self.data[i] != " ") and (self.search[j] != " "):
                     if self.data[i].lower() == self.search[j].lower():
-                        concide_m[i,j] = 1
+                        concide_m[i, j] = 1
         
         return concide_m
     
@@ -30,7 +32,7 @@ class stradar(object):
         
         for i in range(self.data_len):
             for j in range(self.search_len):
-                if concide_m_clear[i,j] == 1:
+                if concide_m_clear[i, j] == 1:
                     dia = 0
                     gp_len = 0
                     while (i+dia < self.data_len) and (j+dia < self.search_len) and (concide_m_clear[i+dia, j+dia] == 1):
@@ -38,44 +40,51 @@ class stradar(object):
                         dia += 1
                     horiz_lines = [y for y in range(i, i + gp_len)]
                     vert_lines = [x for x in range(j, j + gp_len)]
-                    self.groups_list.append((horiz_lines, vert_lines,gp_len))
+                    self.groups_list.append((horiz_lines, vert_lines, gp_len))
                     #чистка
-                    for cl in range (0, gp_len):
-                        concide_m_clear[i + cl,j + cl] = 0
+                    for cl in range(0, gp_len):
+                        concide_m_clear[i + cl, j + cl] = 0
         
         return self.groups_list
   
   
     def groups_clear(self):
             #поиск пересекающихся по горизонтали и вертикали более мелких групп и их отсев
-        groups_sort = sorted(self.groups(), key = lambda group: group[2], reverse = True)
+        groups_sort = sorted(self.groups(), key=lambda group: group[2], reverse=True)
+
         for i, big_group in enumerate(groups_sort):
             horiz_big_set = set(big_group[0])
             vert_big_set = set(big_group[1])
+
+            if i != len(groups_sort):
     
-            for bottom_group in groups_sort[i+1:]:
-                horiz_bottom_set = set(bottom_group[0])
-                vert_bottom_set = set (bottom_group[1])
-                
-                try:
-                    if len(horiz_big_set & horiz_bottom_set) != 0:
+                for bottom_group in groups_sort[i+1:]:
+                    horiz_bottom_set = set(bottom_group[0])
+                    vert_bottom_set = set(bottom_group[1])
+
+                    if (len(horiz_big_set & horiz_bottom_set) != 0) \
+                            or (len(vert_big_set & vert_bottom_set) != 0):
                         groups_sort.remove(bottom_group)
-                    if len(vert_big_set & vert_bottom_set) != 0:
-                        groups_sort.remove(bottom_group)
-                except ValueError as Err:
-                    pass
-        
+
+
         return groups_sort
     
     def result(self):
+        #выходная формула (Сумма квадратов длин оставшихся групп делить на сумму длин искомых слов)
         
         groups_cl = self.groups_clear()
         search_words = self.search.split()
+
+        print(self.search, self.data)
+        pprint(groups_cl)
+     # TODO: усилить значимость положения найденного слова ближе к началу строки для обхода ошибки 5400 rpm
+
+
+        concide_square = [x[2]**(x[2]-1) for x in groups_cl]
+        search_ln = [len(y)**2 for y in search_words]
         
-        concide_square = [x[2]**2 for x in groups_cl]
-        search_square = [len(y)**2 for y in search_words]
-        
-        concide_coeff = (sum(concide_square) / sum(search_square)) ** 0.5
+        concide_coeff = sum(concide_square) / sum(search_ln)
+
   
         return concide_coeff
     
